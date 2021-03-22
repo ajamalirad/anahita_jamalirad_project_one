16b8c169cc4d6732ceacb4f74a7b44a2391494a2
ajamalirad <aplasticlanguage@gmail.com>
2021-03-22 09:32:27 -0400

    added css

---
+++ script.js
@@ -0,0 +1,7 @@
+// when you click on the hamburger menu 
+// the hamburger menu becomes the accent colour
+// the menu items become visible 
+
+// when you click on the hamburger menu again 
+// it becomes black and the menu stops being visible
+
