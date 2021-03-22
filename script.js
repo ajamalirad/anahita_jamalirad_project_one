ca4c909797a1b82267dc81757d1e9f8e66ae1510
ajamalirad <aplasticlanguage@gmail.com>
2021-03-22 13:08:30 -0400

    js added

--- script.js
+++ script.js
@@ -2,8 +2,15 @@
 // the hamburger menu becomes the accent colour
 // the menu items become visible 
 
-// when you click on the hamburger menu again 
+// when you click on the hamburger menu again --> toggle?
 // it becomes black and the menu stops being visible
 
 const menuElement = document.querySelector('.menuLogo');
-const menu = document.querySelector('header nav');
+const menu = document.querySelector('header nav ul');
+
+menuElement.addEventListener('click',function showMenu() {
+    menu.classList.toggle("visibleNav")
+    menuElement.classList.toggle("menuLogoClicked")
+})
+
+
